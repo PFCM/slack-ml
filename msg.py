289cbcb3a9f2b9e99078beca044740fb9bf05acd
Paul Mathews <pfcmathews@gmail.com>
2016-01-04 17:19:38 +1300

    test to see why filtering by username failed

--- msg.py
+++ msg.py
@@ -16,6 +16,6 @@ def new_msg():
         and flask.request.form['user_name'] != USERNAME: # avoid feedback
         # send it straight back
         msg = flask.request.form['text']
-        logging.info('Received message: %s', msg)
-        return json.dumps({'text':msg})
+        logging.info('Received message: %s from %s', msg, flask.request.form['user_name'])
+        return #json.dumps({'text':msg})
     return 'nope', 401
