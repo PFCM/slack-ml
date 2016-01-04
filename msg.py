60893e70a50f936b6a04306f64e4fd84f8d1a269
Paul Mathews <pfcmathews@gmail.com>
2016-01-04 16:54:55 +1300

    actually check incoming messages

--- msg.py
+++ msg.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 @app.route('/msg/new', methods=['POST'])
 def new_msg():
     """Stores new messages"""
-    msg = flask.request.data['text']
-    logging.info('Received message: %s', msg)
-    return {'text':msg}
+    # check it's legit
+    if flask.request.data['token'] == 'BxXxBTiFbTQI3g9fqtowbMOz':
+        msg = flask.request.data['text']
+        logging.info('Received message: %s', msg)
+        return {'text':msg}
+    return 'nope', 401
