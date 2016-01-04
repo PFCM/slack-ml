d3a0c09b68f48ace5fd80a24e7420b8a0416b041
Paul Mathews <pfcmathews@gmail.com>
2016-01-04 17:42:37 +1300

    think that should do it

--- msg.py
+++ msg.py
@@ -6,21 +6,17 @@ import flask
 
 app = Flask(__name__)
 
-USERNAME = "ml" # username of bot on slack
-once = 0
+USERNAME = "ml" # actually its 'slackbot' but docs say this should work, so do both
 
 @app.route('/msg/new', methods=['POST'])
 def new_msg():
     """Stores new messages"""
     # check it's legit
     if flask.request.form['token'] == 'BxXxBTiFbTQI3g9fqtowbMOz' \
-        and flask.request.form['user_name'] != USERNAME: # avoid feedback
+        and flask.request.form['user_name'] != USERNAME \
+        and flask.request.form['user_name'] != 'slackbot': # avoid feedback
         # send it straight back
         msg = flask.request.form['text']
         logging.info('Received message: "%s" from %s', msg, flask.request.form['user_name'])
-        global once
-        if once == 0:
-            once = 1 # this may happen a cupl eof times because threaded but shouldn't explode
-            return json.dumps({'text':msg})
-        return ""#json.dumps({'text':msg})
+        return json.dumps({'text':msg})
     return 'nope', 401
