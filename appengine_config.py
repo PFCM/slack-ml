cd9984fa20479d9195f83d760e6053ea23f230e0
Paul Mathews <pfcmathews@gmail.com>
2016-01-09 10:44:01 +1300

    try and fix import problems for modules

--- appengine_config.py
+++ appengine_config.py
@@ -8,4 +8,4 @@ from google.appengine.ext import vendor
 
 # Third-party libraries are stored in "lib", vendoring will make
 # sure that they are importable by the application.
-vendor.add('lib')
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
