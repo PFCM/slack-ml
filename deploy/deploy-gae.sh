8e3663f55704f87b521d5405a3d9cf81ed889351
Paul Mathews <pfcmathews@gmail.com>
2016-01-09 00:58:34 +1300

    more attempts

--- deploy/deploy-gae.sh
+++ deploy/deploy-gae.sh
@@ -2,11 +2,14 @@
 
 # deploys to app engine
 if [ ! -f ${HOME}/google-cloud-sdk ]; then
+    echo "installing gcloud"
     curl https://sdk.cloud.google.com | bash;
 fi
 # authenticate
+echo "authenticating"
 gcloud auth activate-service-account --key-file client-secret.json
 
 # and deploy, add extra modules here
+echo "deploying"
 gcloud -q preview app deploy app.yaml msg/msg.yaml
 # run end-to-end tests?
